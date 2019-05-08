bf9c84643489093d806621902411b3024ae1fcf4
fengjiangtao <fengjiangt@139.com>
2019-05-09 00:08:45 +0800

    提交搜索相关的代码

--- src/main/java/com/jeremy/study/elasticsearch/controller/ElasticsearchController.java
+++ src/main/java/com/jeremy/study/elasticsearch/controller/ElasticsearchController.java
@@ -2,6 +2,9 @@ package com.jeremy.study.elasticsearch.controller;
 
 import com.jeremy.study.elasticsearch.crawler.MiguReadCrawler;
 import com.jeremy.study.elasticsearch.service.ElasticsearchCRUD;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.elasticsearch.action.search.SearchResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,24 +12,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @RestController
-public class ElasticsearchController {
+public class ElasticsearchController
+{
     private static Logger logger = LoggerFactory.getLogger(ElasticsearchController.class);
 
     @Autowired
     ElasticsearchCRUD elasticsearchCRUDImpl;
+
     SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMddHHmmss");
 
     @RequestMapping("/loadData")
-    public String loadData(@RequestParam(required = false) Integer depth) throws Exception {
+    public String loadData(@RequestParam(required = false) Integer depth)
+        throws Exception
+    {
         depth = depth == null || depth <= 0 ? 4 : depth;
         String format = fmt.format(new Date());
-        MiguReadCrawler miguReadCrawler = new MiguReadCrawler("crawl".concat("\\").concat(format), true, elasticsearchCRUDImpl);
+        MiguReadCrawler miguReadCrawler =
+            new MiguReadCrawler("crawl".concat("\\").concat(format), true, elasticsearchCRUDImpl);
         miguReadCrawler.start(depth);
         return "success";
+    }
+
+    @RequestMapping("/search")
+    public String search()
+    {
+        SearchResponse search = elasticsearchCRUDImpl.search(new String[] {"book"},
+            new String[] {"textBook"},
+            "{\"match\": {\"bookName\":\"天才小毒妃\"}}");
+        return search.toString();
     }
 
 }
